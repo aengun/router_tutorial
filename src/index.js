2a7bbcc7a21d56157b4b3c18427dcda2d21f2d66
aengun <aengun@icloud.com>
2021-05-02 00:21:55 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowswerRouter>
+  <BrowserRouter>
     <App />
-  </BrowswerRouter>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
